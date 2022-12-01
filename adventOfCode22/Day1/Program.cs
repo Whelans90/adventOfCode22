3059a15fec2ba5bdca509904c7bff6c5827cf06e
whelans90 <whelans90@gmail.com>
2022-12-01 14:32:21 -0500

    Day 1

---
+++ adventOfCode22/Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿int mostCalories = 0;
+int currentElfCalories = 0;
+List<int> calories = new List<int>();
+int total = 0;
+
+foreach (string line in System.IO.File.ReadLines(@"C:\Users\sean.whelan\Documents\AdventOfCode\elfCalories.txt"))
+{
+    if (line == "")
+    {
+        //mostCalories = Math.Max(mostCalories, currentElfCalories);
+        calories.Add(currentElfCalories);
+        currentElfCalories = 0;
+        continue;
+    }
+
+    currentElfCalories += Int32.Parse(line);
+}
+
+calories.Add(currentElfCalories);
+calories.Sort();
+calories.Reverse();
+total = calories[0] + calories[1] + calories[2];
+
+Console.WriteLine(total);
+
+Console.ReadKey();
