235338d62240be0d09cca5677e52e5f177f3e347
whelans90 <whelans90@gmail.com>
2022-12-02 15:44:14 -0500

    Created empty project for day3

--- adventOfCode22/Day2/Program.cs
+++ adventOfCode22/Day2/Program.cs
@@ -185,6 +185,5 @@ namespace Day2
 
         }
 
-        
     }
 }
---
+++ adventOfCode22/Day3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{ 
+    class challenge1 { }
+    class challenge2 { }
+    class program 
+    {
+        static void Main(string[] args) 
+        {
+            foreach (string line in System.IO.File.ReadLines(AppContext.BaseDirectory + "")) { }
+        }
+    }
+}
